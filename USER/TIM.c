30273d5fcbcc8179e238cc61bf7663f4105a3b61
wensong <wen_emil@163.com>
2018-07-07 19:50:53 +0800

    get 2 TIM3 pwm output

--- USER/TIM.c
+++ USER/TIM.c
@@ -26,28 +26,23 @@ void TIM3_Init(u16 arr, u16 psc, u16 CCR1_Val, u16 CCR2_Val)
 	
 
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);	//使能定时器3时钟
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);  //使能GPIO外设和AFIO时钟
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);  //使能GPIO外设和AFIO时钟
 	 
  
-  //设置该引脚为复用输出功能,输出TIM3 CH2的PWM脉冲波形	GPIOA.7
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource7,GPIO_AF_TIM3); //GPIOA7复用为定时器3
+    //设置该引脚为复用输出功能,输出TIM3 CH2的PWM脉冲波形	GPIOA.7
+    //这个函数每次只能对应初始化一个IO口
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource6 ,GPIO_AF_TIM3); //GPIOA7复用为定时器3
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource7 ,GPIO_AF_TIM3); //GPIOA7复用为定时器3
+//	GPIO_PinAFConfig(GPIOA,GPIO_PinSource8 ,GPIO_AF_TIM3); //GPIOA7复用为定时器3
+//	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9 ,GPIO_AF_TIM3); //GPIOA7复用为定时器3
 	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;           //GPIOA7
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7 ;           //GPIOA7
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;        //复用功能
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;	//速度100MHz
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;      //推挽复用输出
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;        //上拉
 	GPIO_Init(GPIOA,&GPIO_InitStructure);              //初始化PA7
  
-  //设置该引脚为复用输出功能,输出TIM3 CH2的PWM脉冲波形	GPIOA.7
-	GPIO_PinAFConfig(GPIOA,GPIO_PinSource6,GPIO_AF_TIM3); //GPIOA7复用为定时器3
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;           //GPIOA7
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;        //复用功能
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;	//速度100MHz
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;      //推挽复用输出
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;        //上拉
-	GPIO_Init(GPIOA,&GPIO_InitStructure);              //初始化PA7
     
    //初始化TIM3
 	TIM_TimeBaseStructure.TIM_Period = arr - 1; //设置在下一个更新事件装入活动的自动重装载寄存器周期的值
@@ -73,7 +68,7 @@ void TIM3_Init(u16 arr, u16 psc, u16 CCR1_Val, u16 CCR2_Val)
 	TIM_OC2Init(TIM3, &TIM_OCInitStructure);  //根据T指定的参数初始化外设TIM3 OC2
 
 	TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);  //使能TIM3在CCR2上的预装载寄存器
-	
+
 	TIM_ARRPreloadConfig(TIM3,ENABLE);//ARPE使能 
  
 	TIM_Cmd(TIM3, ENABLE);  //使能TIM3
